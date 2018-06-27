2749e25fe17058f39eb5d711caa3db49918d2209
paolo <paolo@pmazzotta>
2018-06-27 18:08:25 +0200

    fix vari;

--- src/js/payment-option/payment-option.component.js
+++ src/js/payment-option/payment-option.component.js
@@ -6,7 +6,8 @@
     		paymentOption: "<",
     		frequencyLabel: "@",
     		sizeLabel: "@",
-    		amountClass: "@"
+    		amountClass: "@",
+    		discount: "<?",
     	},
 		controller: PaymentOptionCtrl,
 		templateUrl: "/tpls/payment-option/payment-option.tpl"
@@ -17,6 +18,26 @@
     	var ctrl = this;
     	
     	this.$onInit = function(){
+    		
+    		if(ctrl.paymentOption.amount && ctrl.paymentOption.amount.finalAmount){
+    			
+    			ctrl.$$initialAmount = angular.copy(ctrl.paymentOption.amount.initialAmount || ctrl.paymentOption.amount.finalAmount);
+    			ctrl.$$finalAmount = angular.copy(ctrl.paymentOption.amount.finalAmount);
+    			ctrl.$applyDiscount();
+    		}	
+    	};
+    	
+    	this.$applyDiscount = function(){
+    		if(ctrl.discount && ctrl.discount.finalAmount){
+    			var discount = ctrl.discount.type != 'PRICE' ? ctrl.$$initialAmount - ((ctrl.$$initialAmount / 100) * parseFloat(ctrl.discount.finalAmount)) : 0;
+    			ctrl.$$finalAmount = ctrl.$$finalAmount - discount;
+    		}
+    	};
+    	
+    	this.$onChanges = function(changes){
+    		if(changes.discount){
+    			ctrl.$applyDiscount();
+    		}
     	};
     }
 })();
