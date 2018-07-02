077dbc1d72d38eebf9ab69419cd95d0c1c7e1014
scoletti <simone.coletti@chromaitaly.com>
2018-07-02 17:45:02 +0200

    fix e ottimiz invoice;

--- src/js/invoice/invoice.component.js
+++ src/js/invoice/invoice.component.js
@@ -4,17 +4,36 @@
 	angular.module("itaca.components").component("chInvoice", {
 		transclude: true,
 		bindings: {
-			invoice: "<"
+			invoice: "<",
+			onSave: "&?"
     	},
 		controller: InvoiceCtrl,
 		template: "<div flex layout-fill ng-transclude></div>"
 	});
 	
 	 /* @ngInject */
-	 function InvoiceCtrl($scope) {
+	 function InvoiceCtrl($scope, $q) {
 		 var ctrl = this;
 		
 		this.$onInit = function() {
+		};
+		
+		this.$save = function() {
+			var deferred = $q.defer();
+			
+			if (ctrl.onSave && angular.isFunction(ctrl.onSave)) {
+				$q.when(ctrl.onSave({$invoice: ctrl.invoice})).then(function(data) {
+					deferred.resolve(data);
+					
+				}, function(error) {
+					deferred.reject(error);
+				});
+				
+			} else {
+				deferred.resolve();
+			}
+			
+			return deferred.promise;
 		};
 	}
 })();
