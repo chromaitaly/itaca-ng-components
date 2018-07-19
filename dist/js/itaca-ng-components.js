6fd8f8febfa0b0b5eeb088cd8b5447a0bd048bf2
paolo <paolo@pmazzotta>
2018-07-19 18:08:42 +0200

    fix city-autocomplete

--- dist/js/itaca-ng-components.js
+++ dist/js/itaca-ng-components.js
@@ -4240,7 +4240,18 @@ var colorPicker = function() {
             ctrl.selectedItem = null;
             ctrl.precopileSearchText();
         };
-        this.precopileSearchText = function() {};
+        this.precopileSearchText = function() {
+            if (ctrl.ngModel && ctrl.ngModel.address) {
+                address += ctrl.ngModel.city ? ctrl.ngModel.city + ", " : "";
+                address += ctrl.ngModel.province ? ctrl.ngModel.province + ", " : "";
+                address += ctrl.ngModel.country ? ctrl.ngModel.country : "";
+                ctrl.searchText = address;
+            } else if (ctrl.ngModel && ctrl.ngModel.addressComplete) {
+                ctrl.searchText = ctrl.ngModel.addressComplete;
+            } else {
+                ctrl.searchText = null;
+            }
+        };
         this.$querySearch = function(query) {
             return GoogleAPI.cities(query).then(function(response) {
                 return response;
@@ -4251,6 +4262,46 @@ var colorPicker = function() {
                 ctrl.ngModel = null;
                 return;
             }
+            GoogleAPI.placeDetails(place.place_id).then(function(data) {
+                var addressInfo = {};
+                for (var i = 0; i < data.address_components.length; i++) {
+                    var address = data.address_components[i];
+                    var type = address.types[0];
+                    if (type == "route") {
+                        addressInfo.street = address.long_name;
+                        continue;
+                    }
+                    if (type == "street_number") {
+                        addressInfo.number = address.long_name;
+                        continue;
+                    }
+                    if (type == "postal_code") {
+                        addressInfo.zipcode = address.long_name;
+                        continue;
+                    }
+                    if (type == "country") {
+                        addressInfo.country = address.long_name;
+                        continue;
+                    }
+                    if (type == "locality") {
+                        addressInfo.city = address.long_name;
+                        continue;
+                    }
+                    if (type == "administrative_area_level_2") {
+                        addressInfo.province = address.short_name;
+                        continue;
+                    }
+                    if (type == "neighborhood") {
+                        addressInfo.district = address.long_name;
+                        continue;
+                    }
+                }
+                addressInfo.lat = data.geometry.location.lat();
+                addressInfo.lng = data.geometry.location.lng();
+                addressInfo.offset = data.utc_offset ? parseInt(data.utc_offset) * 60 : data.utc_offset;
+                addressInfo.addressComplete = ctrl.selectedItem;
+                ctrl.ngModel = addressInfo;
+            });
         };
     }
 })();
