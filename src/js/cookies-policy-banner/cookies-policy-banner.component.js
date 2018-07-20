1deed5e37502daf04df3e83e86b2668897d7a7f4
paolo <paolo@pmazzotta>
2018-07-20 18:06:57 +0200

    cookies policy banner

---
+++ src/js/cookies-policy-banner/cookies-policy-banner.component.js
@@ -0,0 +1,31 @@
+(function() {
+	'use strict';
+	
+	angular.module("itaca.components").component("chCookiesPolicyBanner", {
+		bindings: {
+			baseUrl: "<?",
+			cookieName: "<?"
+		},
+		controller: CookiesPolicyCtrl,
+		templateUrl: "/tpls/cookies-policy-banner/cookies-policy-banner.tpl"
+			
+	});
+       
+	/* @ngInject */
+    function CookiesPolicyCtrl($scope, $cookies) {
+    	var ctrl = this;
+    	
+    	this.$onInit = function() {
+    		ctrl.cookieName = ctrl.cookieName || 'X-ITACA-COOKIES-POLICY';
+    		
+    		if ($cookies.get(ctrl.cookieName)){
+				ctrl.$$cookiesPolicy = true;
+			}		
+    	};
+    	
+    	this.$acceptCookiesPolicy = function(){
+			 $cookies.put(ctrl.cookieName, 'true', {'expires': moment().add(1, 'years').toDate()});
+			 ctrl.$$cookiesPolicy = true;
+		};
+    }
+})();
