cd91c1eecbe44f8d864370d69198a725dc9b06f4
paolo <paolo@pmazzotta>
2018-07-27 18:05:27 +0200

    ch card material 2; ch statistics chart

--- src/js/chart/chart.component.js
+++ src/js/chart/chart.component.js
@@ -1,107 +1,52 @@
 (function() {
 	'use strict';
 	
-	angular.module("itaca.components").component('chChart', {
+	angular.module("itaca.company").component('chChart', {
 		bindings: {
 			type: "<",
-			values: "<"
+			data: "<",
+			options: "<?"
 		},
 		controller: ChartCtrl,
-		template: 
-			"<canvas id=\"{{ctrl.$$chart.chartType}}\" " +
-				"class=\"{{'chart chart-' + ctrl.$$chart.chartType}}\" " +
-				"chart-data=\"$ctrl.$$chart.data\" " +
-				"chart-labels=\"$ctrl.$$chart.labels\" " +
-				"chart-series=\"$ctrl.$$chart.series\" " +
-				"chart-options=\"$ctrl.$$chart.options\" " +
-				"chart-dataset-override=\"datasetOverride\" " +
-				"chart-click=\"onClick\" " +
-				">" +
-			"</canvas>"
+		template: "<div class=\"chart-container relative\"><canvas class=\"chartjs\"></canvas></div>"
 	});
 
 	/* @ngInject */
-	function ChartCtrl($scope, $element, $attrs, DateUtils, $interval) {
+	function ChartCtrl($scope, $element) {
 		var ctrl = this;
 		
-		
-		this.$$chartTypes = ["line", "bar", "horizontal-bar", "radar", "pie", "polar-area", "doughnut", "bubble"];
-		
-		this.$$types = ["ANNUAL_RESERVATION_AMOUNT_TREND", "ANNUAL_RESERVATION_COUNT_TREND"];
-		
-		this.$onInit = function() {
-			ctrl.$$chart = {};
-			ctrl.$mangageType();
+		this.$onInit = function(){
+			ctrl.type = _.includes(["line", "bar", "horizontal-bar", "radar", "pie", "polar-area", "doughnut", "bubble"], ctrl.type) ? ctrl.type : "line";
+			ctrl.data = ctrl.data || {};
+			ctrl.ctx = $element.find('canvas')[0].getContext('2d');
+			ctrl.$createChart();
 		};
 		
 		this.$onChanges = function(changesObj) {
-			if (changesObj.type) {
-				ctrl.$mangageType();
+			if (changesObj.type && !changesObj.type.isFirstChange()) {
+				ctrl.$createChart();
+			}
+			
+			if (changesObj.data && !changesObj.data.isFirstChange()) {
+				ctrl.$createChart();
+			}
+			
+			if (changesObj.options && !changesObj.options.isFirstChange()) {
+				ctrl.$createChart();
 			}
 		};
 		
-		this.$mangageType = function() {
-			ctrl.type = _.includes(ctrl.$$types, ctrl.type) ? ctrl.type : "ANNUAL_RESERVATION_AMOUNT_TREND";
-			ctrl.$generateChart();
-		};
-		
-		this.$generateChart = function() {
-			var _chart = {
-				labels	: !_.isEmpty(ctrl.values) ? _.keys(ctrl.values)   : {},
-				data 	: !_.isEmpty(ctrl.values) ? _.values(ctrl.values) : {},
-			};
+		this.$createChart = function(){
+			if(!ctrl.ctx){
+				return;
+			}
 			
-			_.forEach(ctrl.$$types, function(type) {
-
-				switch(type) {
-					case "ANNUAL_RESERVATION_AMOUNT_TREND":
-						
-						_chart.series = {};
-						
-						_chart.options = {
-							scales: {
-					            yAxes: [{
-									display: true, // TODO gestire
-									scaleLabel: {
-										display: true,
-										labelString: ""
-									},
-									ticks: {
-										callback: function(value, index, values) {
-					                        return '$' + value;
-					                    }
-									}
-					            }]
-							}
-						};
-						break;
-						
-					case "ANNUAL_RESERVATION_COUNT_TREND":
-						
-						_chart.chartType = ctrl.$$chartTypes[0] //lines
-						_chart.series = {};
-						_chart.options = {
-							scales: {
-					            yAxes: [{
-									display: true, // TODO gestire
-									scaleLabel: {
-										display: true,
-										labelString: ""
-									},
-									ticks: {
-										callback: function(value, index, values) {
-					                        return '$' + value;
-					                    }
-									}
-					            }]
-							}
-						};
-						break;
-				}
+			ctrl.$$chart = new Chart(ctrl.ctx, {
+				type: ctrl.type,
+				data: ctrl.data,
+				options: ctrl.options,
 				
 			});
-			
-			_.assign(ctrl.$$chart, _chart);
 		};
 	}
 })();
