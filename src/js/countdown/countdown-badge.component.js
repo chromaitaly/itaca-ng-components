e9ba8f8ba4fc0b74ff5499a082df1266444fb43f
paolo <paolo@pmazzotta>
2019-02-28 18:06:58 +0100

    countdown

--- src/js/countdown/countdown-badge.component.js
+++ src/js/countdown/countdown-badge.component.js
@@ -1,55 +1,115 @@
 (function() {
 	'use strict';
 	
-	angular.module("itaca-ui").component("countDownBadge", {
+	angular.module("itaca.components").component("countDownBadge", {
 		bindings: {
 			endDate: "<",
-			dateFormat: "@",
+			diameter: "@",
 			message: "@",
-			cssClass: "@"
+			finalMessagge: "@",
+			boxCss: "@",
+			boxFinalCss: "@",
+			hideMessage: "<?",
+			onFinish: "&?"
 		},
 		controller: CountDownBadgeCtrl,
 		template: 
-			"<div layout layout-wrap layout-align=\"space-around\" class=\"{{$ctrl.cssClass}}\">" +
+			"<div id=\"progress-countdown\" layout layout-wrap layout-padding layout-align=\"space-around\" ng-class=\"$ctrl.boxCss\">" +
 				"<div>" +
-				"<md-progress-circular md-mode=\"determinate\" value=\"{{$ctrl.determinateValue}}\"></md-progress-circular>" +
+					"<md-progress-circular md-mode=\"determinate\" value=\"{{$ctrl.$$countdown.progress}}\" md-diameter=\"{{$ctrl.diameter}}\"></md-progress-circular>" +
+					"<div class=\"flex-100 layout-align-center-center layout-column row-1\">" +
+						"<span>{{$ctrl.$$countdown.unit.value}}</span>" +
+						"<small ng-if=\"$ctrl.$$countdown.unit.format\" translate=\"{{$ctrl.$$countdown.unit.format}}\"></small>" +
+					"</div>" +
 				"</div>" +
-				"<div ng-if=\"$ctrl.message\" flex>" +
-					"<span ng-bind=\"$ctrl.message\"></div>" +
+				"<div flex layout=\"column\" layout-align=\"center start\" class=\"md-subhead\" ng-if=\"!$ctrl.hideMessage\">" +
+					"<strong>Hai ancora {{$ctrl.$$countdown.unit.value}} <span translate=\"{{$ctrl.$$countdown.unit.format}}\"></span></strong> " +
+					"<span ng-if=\"$ctrl.message\" ng-bind-html=\"$ctrl.message\"></div>" +
 				"</div>" +
 			"</div>"
 	});
 	
 	 /* @ngInject */
-	function CountDownBadgeCtrl($scope, $mdMedia, $translate, $log, AppOptions, Lang, Dialog, InterestPlaces, Navigator, Notification, Reservation) {
+	function CountDownBadgeCtrl($scope, $mdMedia, $interval) {
 		var ctrl = this;
 		
 		this.$onInit = function(){
 			
-			moment.duration(x.diff(y)).humanize();
+			ctrl.cssClass = ctrl.cssClass || null;
+			ctrl.textCss = ctrl.textCss || null;
 			
-//			var eventTime= 1366549200; // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
-//			var currentTime = 1366547400; // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
-//			var diffTime = eventTime - currentTime;
-//			var duration = moment.duration(diffTime*1000, 'milliseconds');
-//			var interval = 1000;
-//
-//			setInterval(function(){
-//			  duration = moment.duration(duration - interval, 'milliseconds');
-//			    $('.countdown').text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds())
-//			}, interval);
-//			
+			ctrl.hideMessage = _.isBoolean(ctrl.hideMessage) ? ctrl.hideMessage : false;
 			
-			$interval(function() {
-
-		        self.determinateValue += 1;
-		        if (self.determinateValue > 100) {
-		          self.determinateValue = 30;
-		        }
-
-		   }, 100);
+			ctrl.diameter = ctrl.diameter || 100;
 			
+			ctrl.$$countdown = {
+					start: new Date().getTime(),
+					interval: 1000,
+					progress: 100,
+					unit: {
+						value: '!',
+					},
+			};
 			
+			ctrl.$$countdown.end = (ctrl.endDate instanceof Date) ? ctrl.endDate.getTime() : new Date(ctrl.endDate).getTime();
+			ctrl.$$countdown.duration = moment.duration((ctrl.$$countdown.end - ctrl.$$countdown.start), 'milliseconds');
+			
+			ctrl.$intervalFn();
+			
+			ctrl.$$interval = $interval(ctrl.$intervalFn, ctrl.$$countdown.interval);
+		};
+		
+		
+		this.$intervalFn = function(){
+			ctrl.$$countdown.duration = moment.duration(ctrl.$$countdown.duration - ctrl.$$countdown.interval, 'milliseconds');
+			
+			if(ctrl.$$countdown.duration.asMilliseconds() <= 0){
+            	ctrl.$finih();
+            	
+            } else {
+	            ctrl.$getUnit();
+	            ctrl.$getProgress();
+            }
+		};
+		
+		
+		this.$getUnit = function(){
+			ctrl.$$countdown.unit = {};
+			
+			var d = ctrl.$$countdown.duration.days(), h = ctrl.$$countdown.duration.hours(), m = ctrl.$$countdown.duration.minutes(), s = ctrl.$$countdown.duration.seconds();
+			
+			if(d > 0){
+				ctrl.$$countdown.unit.value = d;
+				ctrl.$$countdown.unit.format = d == 1 ? 'date.day' : 'date.days';
+				
+			} else if(h > 0){
+				ctrl.$$countdown.unit.value = h;
+				ctrl.$$countdown.unit.format = h == 1 ? 'date.hour' : 'date.hours';
+				
+			} else if(m > 0){
+				ctrl.$$countdown.unit.value = m;
+				ctrl.$$countdown.unit.format = m == 1 ? 'date.minute' : 'date.minutes';
+				
+			} else {
+				ctrl.$$countdown.unit.value = s;
+				ctrl.$$countdown.unit.format = s == 1 ? 'date.second' : 'date.seconds';
+			}
+			
+		};
+		
+		this.$getProgress = function() {
+			ctrl.$$countdown.progress = 100 * (Math.max(0, ctrl.$$countdown.end - new Date()) / (ctrl.$$countdown.end - ctrl.$$countdown.start));
 		}
+		
+		
+		this.$finih = function(){
+			$interval.cancel(ctrl.$$interval);
+			
+			ctrl.onFinish && ctrl.onFinish();
+		};
+		
+		this.$onDestroy = function(){
+			ctrl.$finih();
+		};
 	}
 })();
